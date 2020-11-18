dc95e92f458513e91c28f54ab080b3bfe86eca4f
njerigathigi <njeriiterry@gmail.com>
2020-11-18 12:05:17 +0300

    updating the reversed function

--- Lists2/gobackwards.py
+++ Lists2/gobackwards.py
@@ -42,3 +42,25 @@ for index, value in enumerate(reversed(data)):
 #                 del data[index]
 
 # print(data)
+
+
+#The reversed() function returns the reversed iterator of the given sequence.
+#The syntax of reversed() is:
+# reversed(seq)
+#The reversed() function takes a single parameter:
+#seq - the sequence to be reversed
+# # for string
+# seq_string = 'Python'
+# print(list(reversed(seq_string)))
+
+# # for tuple
+# seq_tuple = ('P', 'y', 't', 'h', 'o', 'n')
+# print(list(reversed(seq_tuple)))
+
+# # for range
+# seq_range = range(5, 9)
+# print(list(reversed(seq_range)))
+
+# # for list
+# seq_list = [1, 2, 4, 3, 5]
+# print(list(reversed(seq_list)))
